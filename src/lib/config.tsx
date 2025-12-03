d3fd54fa43b5238b7696f5fe4fc3ea0878d052da
thev1ndu <itsthw9@gmail.com>
2025-12-03 18:33:16 +0530

    removed globe

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -3,7 +3,7 @@ import { FourthBentoAnimation } from "@/components/fourth-bento-animation";
 import { SecondBentoAnimation } from "@/components/second-bento-animation";
 import { ThirdBentoAnimation } from "@/components/third-bento-animation";
 import { FlickeringGrid } from "@/components/ui/flickering-grid";
-import { Globe } from "@/components/ui/globe";
+// import { Globe } from "@/components/ui/globe";
 import { cn } from "@/lib/utils";
 
 export const Highlight = ({
@@ -673,7 +673,7 @@ export const siteConfig = {
         id: 2,
         content: (
           <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden [mask-image:linear-gradient(to_top,transparent,black_50%)] -translate-y-20">
-            <Globe className="top-28" />
+            {/* <Globe className="top-28" /> */}
           </div>
         ),
 
